26cd81cd05de818e9aad35873e2ef68be0710fb2
kyokukaityou <35418762+kyokukaityou@users.noreply.github.com>
2018-03-10 18:10:33 +0900

    M_news_information
    M_news_information

---
+++ 02_练习/M_news_information.cs
@@ -0,0 +1,19 @@
+// 受取パラメータの取得
+String id = "";
+if (HttpContext.Current.Request.QueryString[NewsTable.ID] != null)
+{
+  id = HttpContext.Current.Request.QueryString[NewsTable.ID].ToString();
+}
+
+// データの取得
+DBTable dbt = new DBTable();
+// 企業情報
+DataTable company = dbt.Ns_company;
+this.company_tel.InnerText = company.Rows[0][CompanyTable.TEL].ToString();
+this.company_fax.InnerText = company.Rows[0][CompanyTable.FAX].ToString();
+this.company_adress.InnerText = company.Rows[0][CompanyTable.ADDRESS].ToString();
+this.company_mail.InnerText = company.Rows[0][CompanyTable.MAIL].ToString();
+// 行データ取得
+DataRow dr = dbt.GetDataRow(dbt.Ns_news, id);
+this.title.InnerText = dr[NewsTable.TITLE].ToString();
+this.content.InnerHtml = dr[NewsTable.CONTENT].ToString();
